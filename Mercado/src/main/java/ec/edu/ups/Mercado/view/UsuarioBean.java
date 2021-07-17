a4fbb6f6b8f7b055099d5fd6f2f6c3417da7e52e
ShootDomy <ShootDomy@DESKTOP-9CU946S>
2021-07-16 19:34:22 -0500

    Creacion de UsuarioBean, usuarioOn y usuarioDAO

---
+++ Mercado/src/main/java/ec/edu/ups/Mercado/view/UsuarioBean.java
@@ -0,0 +1,55 @@
+package ec.edu.ups.Mercado.view;
+
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import ec.edu.ups.Mercado.bussines.UsuariosON;
+import ec.edu.ups.Mercado.modelo.Usuario;
+
+@Named
+@RequestScoped
+public class UsuarioBean {
+
+	private Usuario newUsuario = new Usuario();
+	private List<Usuario> usuarios;
+	
+	@Inject
+	private UsuariosON onUsuario;
+	
+	@PostConstruct
+	public void init() {
+		usuarios = onUsuario.getUsuarios();
+		System.out.println(usuarios);
+	}
+
+	public Usuario getNewUsuario() {
+		return newUsuario;
+	}
+
+	public void setNewUsuario(Usuario newUsuario) {
+		this.newUsuario = newUsuario;
+	}
+
+	public List<Usuario> getUsuarios() {
+		return usuarios;
+	}
+
+	public void setUsuarios(List<Usuario> usuarios) {
+		this.usuarios = usuarios;
+	}
+	
+	public String guardarUsuario() {
+		try {
+			onUsuario.insertarUsuario(newUsuario);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return "lista-usuario?faces-redirect=true";
+	}
+	
+}
