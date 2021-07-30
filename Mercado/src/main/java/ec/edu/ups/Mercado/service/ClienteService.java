7551bc99a6ffb3f594837c18660af0f9a0e5004b
ShootDomy <ShootDomy@DESKTOP-9CU946S>
2021-07-30 11:34:44 -0500

    Servicio web para clientes

---
+++ Mercado/src/main/java/ec/edu/ups/Mercado/service/ClienteService.java
@@ -0,0 +1,33 @@
+package ec.edu.ups.Mercado.service;
+
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import ec.edu.ups.Mercado.bussines.UsuariosON;
+import ec.edu.ups.Mercado.modelo.Usuario;
+
+@Path("Clientes")
+public class ClienteService {
+
+	@Inject
+	private UsuariosON usuarioON;
+	
+	@PUT
+	@Path("RegistroCliente")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public String registrarCliente(Usuario u) {
+		try {
+			usuarioON.insertarUsuario(u);
+			return "Ok";
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return "Error";
+		}
+	}
+}
