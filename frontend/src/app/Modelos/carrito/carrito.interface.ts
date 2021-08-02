8f355ed33fe6e4c029a9ce6d2cbaf96381bc73d6
Byron Godoy <User@DESKTOP-9CU123S>
2021-08-02 16:16:00 -0500

    Inicio Carrito

---
+++ frontend/src/app/Modelos/carrito/carrito.interface.ts
@@ -0,0 +1,9 @@
+export interface CarritoI {
+  codigo: number;
+  codProd: String;
+  nombreProd: String;
+  cantidad: number;
+  precioU: number;
+  total: number;
+  codFac: number;
+}
---
+++ frontend/src/app/domain/carrito.ts
@@ -0,0 +1,9 @@
+export class Carrito {
+  codigo: number = 0;
+  codProd: String = '';
+  nombreProd: String = '';
+  cantidad: number = 0;
+  precioU: number = 0;
+  total: number = 0;
+  codFac: number = 0;
+}
