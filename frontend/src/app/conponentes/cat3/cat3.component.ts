0015240bdaaf4d23ec68c64a6329e4a29322f8df
ShootDomy <ShootDomy@DESKTOP-9CU946S>
2021-08-08 15:55:36 -0500

    Agregar mensajes en codigo y formato

--- frontend/src/app/conponentes/cat3/cat3.component.ts
+++ frontend/src/app/conponentes/cat3/cat3.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from "@angular/router";
 import {ProductoService} from "../../services/producto.service";
+import {CarritoService} from "../../services/carrito.service";
 
 @Component({
   selector: 'app-cat3',
@@ -11,10 +12,21 @@ export class Cat3Component implements OnInit {
 
   productos: any;
   constructor(private router: Router,
-              private productoService: ProductoService) { }
+              private productoService: ProductoService,
+              private carritoService: CarritoService) { }
 
   ngOnInit(): void {
     this.productos = this.productoService.getCat3Prod();
   }
 
+  addCarrito(codigo: number){
+
+    var d=(document.getElementById(""+codigo) as HTMLInputElement).value;
+
+    var num=parseInt(d);
+    console.log(num);
+    this.carritoService.addCarrito(codigo,num);
+    console.log("Producto Añadido " + codigo)
+
+  }
 }
